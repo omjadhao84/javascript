7df65e2c3511e4780f43c15e10a182143d3e4f49
Om Jadhao <91279302+omjadhao84@users.noreply.github.com>
2023-10-27 17:12:17 +0530

    Create three.js

---
+++ 05_iterations.js/three.js
@@ -0,0 +1,33 @@
+/*for of
+['','','']
+[{},{},{}]
+const arr=[4,6,7,8,6]
+for(const num of arr){
+    console.log(num);
+}
+const greetings="hello world!"
+for(const greet of greetings){
+    console.log(`character of string ${greet}`)
+}
+*/
+//********************MAPS*********************** */
+ const map=new Map()
+ map.set('IN',"India")
+ map.set("JPN","Japan")
+ map.set("FR","France")
+ //console.log(map)
+
+ for(const [key,value] of map){
+    console.log(key,":--",value)
+ }
+
+ const myObject={
+    'game1':"NFS",
+    'game2':"Spiderman"
+ }
+
+ for(const [key,value]of myObject){
+    console.log(key,CSSMathValue)//we cant iterate objects in this way
+
+ }
+ 
