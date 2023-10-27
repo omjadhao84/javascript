33afd1d5e0a248a6c23ac1cf68ee3d6cd518dd79
Om Jadhao <91279302+omjadhao84@users.noreply.github.com>
2023-10-27 16:41:02 +0530

    Create two.js

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,22 @@
+//while
+/*
+let i=0;
+while(i<10){
+    console.log(`Value of i is ${i}`);
+    i++;
+}
+*/
+/*
+let myArray=['flash','superman','batman'];
+ let j=0;
+while(myArray.length>j){
+      console.log(myArray[j]);
+      j++;
+}
+*/
+
+let score=1;
+do{
+    console.log(`score is ${score}`)
+     score++
+}while(score<5)
